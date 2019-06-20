632a6db32329ff5563574cd956eeea42f8a69574
iamvee <naeini.v@gmail.com>
2019-06-20 18:47:09 +0330

    collection operators

---
+++ S02/collection_operators/convert_collection_types.py
@@ -0,0 +1,3 @@
+lst = [1, 2, 3]
+out = tuple(lst)
+print(out)
---
+++ S02/collection_operators/list_and_tuple_operators.py
@@ -0,0 +1,22 @@
+list_one = [1, 2, 3]
+list_two = [5, 6, 7]
+
+list_three = list_one + list_two
+print(list_three)
+
+list_new = list_one * 10
+print(list_new)
+
+tup_1 = (1, 2, 3)
+tup_2 = (10, 20, 30)
+tup_3 = tup_1 + tup_2
+print(tup_3)
+
+text_1 = "hello"
+text_2 = " "
+text_3 = "world"
+result = text_1 + text_2 + text_3
+print(result)
+
+
+
