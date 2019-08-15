add8fe61ae0ce33f069dd3ac754a9cfb468a41f1
iamvee <naeini.v@gmail.com>
2019-08-15 18:23:44 +0330

    flask

--- S10/a_simple_web_service/app.py
+++ S10/a_simple_web_service/app.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("my-app")
 
+#
+# @app.route("/")
+# def index():
+#     return render_template("main.html")
+#
+#
+# @app.route("/login/<name>/<int:var>")
+# def login_page(name, var):
+#     return render_template("login.html", name=name, var=var)
+#
+#
+# @app.route("/table/<int:cols>/<int:rows>")
+# def table(cols, rows):
+#     return render_template("table.html", rows=rows, cols=cols)
 
-@app.route("/")
-def index():
-    with open('main.html') as f:
-        return f.read()
 
-
-@app.route("/login")
-def login_pate():
-    return "login page"
-
+@app.route("/cool", methods=["GET", "POST"])
+def cool():
+    if request.method == 'GET':
+        return render_template("cool.html")
+    else:
+        weight = float(request.form['w'])
+        height = float(request.form['h'])
+        bmi = weight / height ** 2
+        return render_template("cool.html", bmi=bmi)
 
 app.run()
