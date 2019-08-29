cd9b27cb5328868a1b2ef024dfdc3904e8258fa7
iamvee <naeini.v@gmail.com>
2019-08-29 14:05:35 +0330

    scientific python : numpy

---
+++ S12/ndim_arrays_numpy/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# class MyCollection:
+#     def __init__(self, inp):
+#         self.args = inp
+#
+#     def __getitem__(self, item):
+#         return self.args[item[0]][item[1]][item[2]]
+#
+#
+# m = MyCollection([[1, 2 ,3], [4, 5, 6], [7 , 8, 9]])
+# print(m[-1, -1, -1])
+#
+
+
+arr = np.array([[1, 2 ,3], [4, 5, 6], [7 , 8, 9]])
+print(arr)
